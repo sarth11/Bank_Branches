a3f65f3969fa8e73a8b49ee419d11da70dcac0ca
Sarthak Soni <32573186+sarth11@users.noreply.github.com>
2019-04-20 14:21:20 +0530

    Add files via upload

--- Bank-Branches/src/main/java/com/fyle/bank/repository/BankBranchRepository.java
+++ Bank-Branches/src/main/java/com/fyle/bank/repository/BankBranchRepository.java
@@ -2,12 +2,12 @@ package com.fyle.bank.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import com.fyle.bank.model.BankBranches;
 
-public interface BankBranchRepository extends JpaRepository<BankBranches, Long>{
+public interface BankBranchRepository extends CrudRepository<BankBranches, Long>{
 	
 	@Query(value="SELECT * FROM bank_branches WHERE ifsc = ?",nativeQuery=true) 
 	public BankBranches findByIfsc(String ifsc);
